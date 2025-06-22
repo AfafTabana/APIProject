95bc63d6b94af77d389070b058739aceeefa96f7
AfafTabana <96386311+AfafTabana@users.noreply.github.com>
2025-06-23 00:24:55 +0300

    Solved

--- APIProject/UnitofWork/UnitOfWork.cs
+++ APIProject/UnitofWork/UnitOfWork.cs
@@ -8,6 +8,9 @@ namespace APIProject.UnitofWork
         public readonly SystemMangmentContext _db;
         private GenericRepository<Student> _studentrepo;
         public UnitOfWork( SystemMangmentContext db)
+        private GenericRepository<Question> _Questionrepo;
+        private GenericRepository<Exam> _examrepo;
+        public UnitOfWork( SystemMangmentContext db)
         {
             this._db = db;
         }
@@ -21,6 +24,43 @@ namespace APIProject.UnitofWork
                     _studentrepo = new GenericRepository<Student>(_db);
                 }
                 return _studentrepo;
+            }
+        }
+
+        public GenericRepository<Question> Questionrepo
+        {
+            get
+            {
+                if (_Questionrepo == null) {
+
+                    _Questionrepo = new GenericRepository<Question>(_db);
+
+
+                }
+
+                return _Questionrepo;
+
+            }
+        }
+
+        public GenericRepository<Exam> ExamRepo
+        {
+            get
+            {
+                if (_examrepo == null)
+                {
+                    _examrepo = new GenericRepository<Exam>(_db);
+                }
+                return _examrepo;
+            }
+        }
+
+        {
+            get
+            {
+                if (_studentExamRepo == null)
+                    _studentExamRepo = new GenericRepository<Student_Exam>(context);
+                return _studentExamRepo;
             }
         }
 
